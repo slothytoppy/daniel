61e6b73a4ee6c719a57f3b5c2416c049b0d4bdd7
slothytoppy <jacobcalvining@gmail.com>
2025-07-20 01:33:48 -0500

    init of daniel

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            rustup
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use fuse::*;
+
+fn main() {
+    println!("Hello, world!");
+}
